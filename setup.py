421e6dbea61526d620e62eb2413708fd23eac4ba
Daniel Martins <daniel.tritone@gmail.com>
2008-10-27 22:04:10 -0200

    Adding easy_install setup.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'unittest-xml-reporting',
+    version = '1.0',
+    author = 'Daniel Fernandes Martins',
+    author_email = 'daniel.tritone@gmail.com',
+    description = 'PyUnit-based test runner with JUnit like XML reporting.',
+    long_description = 'PyUnit-based test runner with JUnit like XML reporting.',
+    license = 'LGPL',
+    platforms = ['Any'],
+    keywords = ['pyunit', 'unittest', 'junit xml', 'report', 'testrunner'],
+    url = 'http://github.com/danielfm/unittest-xml-reporting/tree/master/',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Text Processing :: Markup :: XML'
+    ],
+    py_modules = ['xmlrunner'],
+    test_suite = 'xmlrunner_tests',
+)
+
