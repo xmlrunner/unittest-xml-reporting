7a33c6962c7e49bdc091fc4b4e9307e9e7b3ade6
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-09-26 20:22:47 -0300

    Fixed syntax error on classifiers section.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'unittest-xml-reporting',
-    version = '1.3.2',
+    version = '1.4',
     author = 'Daniel Fernandes Martins',
     author_email = 'daniel.tritone@gmail.com',
     description = 'PyUnit-based test runner with JUnit like XML reporting.',
@@ -19,7 +19,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Testing'
