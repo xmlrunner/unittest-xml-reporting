dc86cc933e67bd79ea611ed961026ad238102b3a
Pedro Algarvio <pedro@algarvio.me>
2013-09-06 20:28:28 +0100

    Use `unittest2` if available.

--- src/xmlrunner/__init__.py
+++ src/xmlrunner/__init__.py
@@ -8,7 +8,12 @@ default TextTestRunner.
 import os
 import sys
 import time
-from unittest import TestResult, _TextTestResult, TextTestRunner
+try:
+    from unittest2.runner import TextTestRunner
+    from unittest2.runner import TextTestResult as _TextTestResult
+    from unittest2.result import TestResult
+except ImportError:
+    from unittest import TestResult, _TextTestResult, TextTestRunner
 
 try:
     # Removed in Python 3
