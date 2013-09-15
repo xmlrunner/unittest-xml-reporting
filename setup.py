57213d9bc88b796b736bb161c52251ae7e21083b
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2013-09-15 18:23:58 -0300

    It's a new beginning. :-)

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'unittest-xml-reporting',
+    version = '2.0.0',
+    author = 'Daniel Fernandes Martins',
+    author_email = 'daniel.tritone@gmail.com',
+    description = 'PyUnit-based test runner with JUnit like XML reporting.',
+    license = 'LGPL',
+    platforms = ['Any'],
+    keywords = ['pyunit', 'unittest', 'junit xml', 'report', 'testrunner'],
+    url = 'http://github.com/danielfm/unittest-xml-reporting/tree/master/',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing'
+    ],
+    package_dir = {'':'src'},
+    test_suite = 'xmlrunner.tests.testsuite',
+    tests_require = ['mock']
+)
