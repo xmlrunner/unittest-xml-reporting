bccea94ae1cfca7f7f87f702e7d767096275b290
Damien Nozay <damien.nozay@gmail.com>
2014-10-23 14:22:35 -0700

    add tests for expected failures / unexpected success

--- src/xmlrunner/tests/testsuite.py
+++ src/xmlrunner/tests/testsuite.py
@@ -28,6 +28,14 @@ class XMLTestRunnerTestCase(unittest.TestCase):
             pass
         def test_fail(self):
             self.assertTrue(False)
+        @unittest.expectedFailure
+        def test_expected_failure(self):
+            self.assertTrue(False)
+        @unittest.expectedFailure
+        def test_unexpected_success(self):
+            pass
+        def test_error(self):
+            1 / 0
 
     def test_xmlrunner(self):
         stream = StringIO()
@@ -39,6 +47,21 @@ class XMLTestRunnerTestCase(unittest.TestCase):
         suite.addTest(self.DummyTest('test_pass'))
         suite.addTest(self.DummyTest('test_skip'))
         suite.addTest(self.DummyTest('test_fail'))
+        suite.addTest(self.DummyTest('test_expected_failure'))
+        suite.addTest(self.DummyTest('test_unexpected_success'))
+        suite.addTest(self.DummyTest('test_error'))
+        self.assertEqual(0, len(glob(os.path.join(outdir, '*xml'))))
+        runner.run(suite)
+        self.assertEqual(1, len(glob(os.path.join(outdir, '*xml'))))
+
+    def test_xmlrunner_pass(self):
+        stream = StringIO()
+        outdir = mkdtemp()
+        self.addCleanup(rmtree, outdir)
+        runner = xmlrunner.XMLTestRunner(
+            stream=stream, output=outdir, verbosity=0)
+        suite = unittest.TestSuite()
+        suite.addTest(self.DummyTest('test_pass'))
         self.assertEqual(0, len(glob(os.path.join(outdir, '*xml'))))
         runner.run(suite)
         self.assertEqual(1, len(glob(os.path.join(outdir, '*xml'))))
