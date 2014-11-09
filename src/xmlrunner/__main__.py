c847ac4dfc87228b52f2ff3e1d05dabc0abffca3
Damien Nozay <damien.nozay@gmail.com>
2014-11-08 22:50:23 -0800

    hide unnecessary options from main() (see issue #59)

---
+++ src/xmlrunner/__main__.py
@@ -0,0 +1,29 @@
+"""Main entry point"""
+
+import sys
+if sys.argv[0].endswith("__main__.py"):
+    import os.path
+    # We change sys.argv[0] to make help message more useful
+    # use executable without path, unquoted
+    # (it's just a hint anyway)
+    # (if you have spaces in your executable you get what you deserve!)
+    executable = os.path.basename(sys.executable)
+    sys.argv[0] = executable + " -m xmlrunner"
+    del os
+
+__unittest = True
+
+try:
+    from unittest2.main import TestProgram
+except ImportError:
+    from unittest.main import TestProgram, USAGE_AS_MAIN
+    TestProgram.USAGE = USAGE_AS_MAIN
+
+from .runner import XMLTestRunner
+
+main = TestProgram
+
+main(
+    module=None, testRunner=XMLTestRunner,
+    # see issue #59
+    failfast=False, catchbreak=False, buffer=False)
