497d778e9c26edba714fad9f802535979a2896f1
Damien Nozay <damiennozay+github@gmail.com>
2020-02-17 16:29:00 -0800

    whitespace

--- xmlrunner/extra/xunit_plugin.py
+++ xmlrunner/extra/xunit_plugin.py
@@ -4,19 +4,19 @@ import lxml.etree as etree
 
 TRANSFORM = etree.XSLT(etree.XML('''\
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
-	<xsl:output method="xml" indent="yes" />
+    <xsl:output method="xml" indent="yes" />
 
     <!-- /dev/null for these attributes -->
-	<xsl:template match="//testcase/@file" />
-	<xsl:template match="//testcase/@line" />
-	<xsl:template match="//testcase/@timestamp" />
+    <xsl:template match="//testcase/@file" />
+    <xsl:template match="//testcase/@line" />
+    <xsl:template match="//testcase/@timestamp" />
 
     <!-- copy the rest -->
-	<xsl:template match="node()|@*">
-		<xsl:copy>
-			<xsl:apply-templates select="node()|@*" />
-		</xsl:copy>
-	</xsl:template>
+    <xsl:template match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*" />
+        </xsl:copy>
+    </xsl:template>
 </xsl:stylesheet>'''))
 
 
