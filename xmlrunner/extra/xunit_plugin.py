6ad83e2e5c20ec3735280666c352642ca3915a97
Damien Nozay <damiennozay+github@gmail.com>
2020-02-19 18:41:41 -0800

    try to fix pypy build (#216)

--- xmlrunner/extra/xunit_plugin.py
+++ xmlrunner/extra/xunit_plugin.py
@@ -2,7 +2,8 @@ import io
 import lxml.etree as etree
 
 
-TRANSFORM = etree.XSLT(etree.XML('''\
+TRANSFORM = etree.XSLT(etree.XML(b'''\
+<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
     <xsl:output method="xml" indent="yes" />
 
