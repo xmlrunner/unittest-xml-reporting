0205b60b206f93d0dcdd8eef92df03cff590c914
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-05 05:49:59 +0800

    Delete tests directory

--- tests/django_example/app/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models  # NOQA
-
-# Create your models here.
--- tests/django_example/app2/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-# Create your tests here.
-class DummyTestCase(TestCase):
-    def test_pass(self):
-        pass
