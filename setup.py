c43427611390fba83ca13fbb5311bd8fece5048f
Damien Nozay <damiennozay+github@gmail.com>
2022-01-19 07:41:25 -0800

    Add LICENSE to tarball
    should fix #259

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     description = 'unittest-based test runner with Ant/JUnit like XML reporting.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
+    data_files = [('', ['LICENSE'])],
     install_requires = ['lxml'],
     license = 'BSD',
     platforms = ['Any'],
